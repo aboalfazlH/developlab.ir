6ac33476654b134548ffb92a71e648c9f88d9a5f
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-04-21 10:21:11 +0330

    Add models to Admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from .models import Account
 from django.contrib.auth.admin import UserAdmin
 from .forms import AccountChangeForm,AccountCreationForm
 
-
+@admin.register(Account)
 class AccountAdmin(UserAdmin):
     form = AccountChangeForm
     add_form = AccountCreationForm
